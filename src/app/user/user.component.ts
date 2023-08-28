161e2409ad43d2a5aa324feb3228cb05be113e40
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-08-28 12:42:21 +0530

    28 aug 2023
    1242pm

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  providers : [UserService]
+  providers : [UserService, DataService]
 })
 export class UserComponent implements OnInit{
   user: { name: string; } | undefined;
   isLoggedIn = false;  
-
-  constructor(private userService : UserService){
+  data : string | undefined;
+  constructor(private userService : UserService, private dataService : DataService){
     
   }
   ngOnInit(): void {
     this.user = this.userService.user;
+    this.dataService.getDetails().then((data : any) => this.data = data);
   }
 }
